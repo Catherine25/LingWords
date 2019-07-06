b88186b2e79ef827ed176216ac656b1aabbc8116
Catherine <Catherine25@users.noreply.github.com>
2019-07-06 10:22:19 +0300

    Add project files.

---
+++ LingWords/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LingWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
