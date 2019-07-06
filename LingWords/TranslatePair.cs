f67fdf2ffd0f146d21e9c21940dc107a735ee59e
Catherine <Catherine25@users.noreply.github.com>
2019-07-06 14:37:30 +0300

    Add constructor

--- LingWords/TranslatePair.cs
+++ LingWords/TranslatePair.cs
@@ -2,6 +2,12 @@
 {
     class TranslatePair
     {
+        public TranslatePair(string _foreign, string _native)
+        {
+            foreign = _foreign;
+            native = _native;
+        }
+
         public string foreign, native;
     }
 }
