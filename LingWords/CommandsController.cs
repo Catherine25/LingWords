3dc68290f8123fa4a7ab76ccac6c438bcf7b4b09
Catherine <Catherine25@users.noreply.github.com>
2019-07-06 12:27:46 +0300

    Add CommandsController

---
+++ LingWords/CommandsController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LingWords
+{
+    class CommandsController
+    {
+        private string[] CommandString = new string[]
+        {
+            "/start",
+            "/end",
+            "/help",
+        };
+
+        Dictionary<string, Delegate> keyValuePairs = new Dictionary<string, Delegate>();
+
+        public CommandsController()
+        {
+            keyValuePairs.Add("/help",
+                new Action( () => WriteCommands()));
+        }
+
+        public void ReadCommand(string s)
+        {
+            int length = CommandString.Length;
+
+            for (int i = 0; i < length; i++)
+                if (CommandString[i] == s)
+                {
+                    Console.WriteLine();
+
+                    keyValuePairs[s].DynamicInvoke();
+
+                    Console.WriteLine();
+                }
+        }
+
+        public void WriteCommands()
+        {
+            int length = CommandString.Length;
+
+            for (int i = 0; i < length; i++)
+                Console.WriteLine(CommandString[i]);
+        }        
+    }
+}
