9a1a5a7cc2e11c144ff3f05b6a764383ed8d8dd7
Catherine <Catherine25@users.noreply.github.com>
2019-07-06 12:32:58 +0300

    Add while loop

--- LingWords/Program.cs
+++ LingWords/Program.cs
@@ -6,7 +6,18 @@ namespace LingWords
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            CommandsController commandsController = new CommandsController();
+            Console.WriteLine("Hello!");
+
+            string input = "";
+
+            while (input != "/exit")
+            {
+                input = Console.ReadLine();
+
+                if (input.StartsWith('/'))
+                    commandsController.ReadCommand(input);
+            }
         }
     }
 }
