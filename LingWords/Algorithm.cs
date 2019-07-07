8951d38fc631b650c207cd81666b325230451ac6
Catherine <Catherine25@users.noreply.github.com>
2019-07-07 23:04:37 +0300

    Add GetMaxNumberFromList()

--- LingWords/Algorithm.cs
+++ LingWords/Algorithm.cs
@@ -1,7 +1,19 @@
-﻿namespace LingWords
+﻿using System.Collections.Generic;
+
+namespace LingWords
 {
     static class Algorithm
     {
+        public static int GetMaxNumberFromList(IList<TranslatePair> list)
+        {
+            int length = list.Count;
+            int max = 0;
 
+            for (int i = 0; i < length; i++)
+                if (max < list[i].studyRating)
+                    max = list[i].studyRating;
+
+            return max;
+        }
     }
 }
